33f1d648d5d609528d0be4586189c0ca1a3a5f25
Korneliu <olbiton@gmail.com>
2021-12-27 15:44:22 -0800

    fixed response object

--- server/index.js
+++ server/index.js
@@ -34,8 +34,8 @@ app.post('/api/auth/sign-up', (req, res, next) => {
       const params = [username, hashedPassword];
       db.query(sql, params)
         .then(result => {
-          const [newUser] = result.rows;
-          res.status(201).json({ newUser });
+          const { userId, username, createdAt } = result.rows[0];
+          res.status(201).json({ userId, username, createdAt });
         })
         .catch(err => next(err));
     });
