7078ad0bfa533baae51f4263a2aa3d9b5f2422dc
Korneliu <olbiton@gmail.com>
2021-12-29 13:04:31 -0800

    aded pages auth,home,notfound and components auth-form,navbar

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import decodeToken from './lib/decode-token.js';
-import SignUpForm from './components/signUpForm';
-import SignInForm from './components/signInForm';
+import parseRoute from './lib/parse-route.jsx';
+import Auth from './pages/auth';
+import Home from './pages/home';
 
 const myToken = React.createContext();
 
@@ -16,6 +17,11 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: parseRoute(window.location.hash)
+      });
+    });
     const token = window.localStorage.getItem('TravelApp-token');
     const user = token ? decodeToken(token) : null;
     this.setState({ user, isAuthorizing: false });
@@ -27,15 +33,28 @@ export default class App extends React.Component {
     this.setState({ user });
   }
 
+  handleSignOut() {
+    window.localStorage.removeItem('TravelApp-token');
+    this.setState({ user: null });
+  }
+
+  renderPage() {
+    const { path } = this.state.route;
+    if (path === '') {
+      return <Home />;
+    }
+    if (path === 'sign-in' || path === 'sign-up') {
+      return <Auth />;
+    }
+    return <NotFound />;
+  }
+
   render() {
     const { user } = this.state;
     const { handleSignIn } = this;
     const contextValue = { user, handleSignIn };
     return (
-      <myToken.Provider value = {contextValue}>
-          <SignUpForm />,
-          <SignInForm />
-      </myToken.Provider>
+      <Home />
     );
   }
 }
