e42a75a846ea9d5daf75773a8b1d87321da918eb
Korneliu <olbiton@gmail.com>
2022-01-03 13:17:22 -0800

    added styling for mobile

--- client/pages/trip-details.jsx
+++ client/pages/trip-details.jsx
@@ -33,26 +33,27 @@ export default class TripDetails extends React.Component {
       <div className='container'>
       <Navbar handleChange={this.handleChange}
               handleSubmit={this.handleSubmit}
-              searchBox={this.state.searchBox}/>
+              searchBox={this.state.searchBox}
+      />
       <div className='row padding-top3 detailed-view-container'>
-        <div className='image-item column-width50'>
-          <div className='image-container'><img className="photo" src={mainPhotoUrl} alt={cityName}></img></div>
+        <div className='image-container column-width50'>
+          <img className="photo" src={mainPhotoUrl} alt={cityName}></img>
         </div>
-        <div className='row column-width50'>
-        <div className='column-width25'>
-          <div className='score-container'>
-            <p className='score-text'>Things to Do - {thingsTodoScore}</p>
-            <p className='score-text'>Food - {foodScore}</p>
-            <p className='score-text'>People - {peopleScore}</p>
-            <p className='score-text'>Transport - {transportScore}</p>
-            <p className='score-text'>Safety - {safetyScore}</p>
+        <div className='horizontal column-width50'>
+          <div className='column-width25'>
+            <div className='score-container'>
+              <p className='score-text'>Things to Do - {thingsTodoScore}</p>
+              <p className='score-text'>Food - {foodScore}</p>
+              <p className='score-text'>People - {peopleScore}</p>
+              <p className='score-text'>Transport - {transportScore}</p>
+              <p className='score-text'>Safety - {safetyScore}</p>
+            </div>
           </div>
-        </div>
-        <div className='column-width25'>
-          <div className="detailed-text-container">
-      <p className='country-name'>{countryName}-<span className='city-name'>{cityName}</span></p>
-      <span className='city-name'>@{username}</span></div>
-        </div>
+          <div className='column-width25'>
+            <div className="detailed-text-container">
+              <p className='country-name'>{countryName}-<span className='city-name'>{cityName}</span></p>
+              <span className='city-name'>@{username}</span></div>
+          </div>
         </div>
       </div>
       <div className='row detailed-view-container'>
