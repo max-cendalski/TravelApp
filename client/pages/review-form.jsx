4a75e5d8b7e18d91b7d093be671a3eee21324c58
Korneliu <olbiton@gmail.com>
2022-01-06 23:06:08 -0800

    completed users-can-write-their-own-reviews

--- client/pages/review-form.jsx
+++ client/pages/review-form.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Navbar from '../components/navbar';
 export default class ReviewForm extends React.Component {
   constructor(props) {
     super(props);
@@ -9,6 +9,7 @@ export default class ReviewForm extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCancelTripReview = this.handleCancelTripReview.bind(this);
   }
 
   componentDidMount() {
@@ -43,12 +44,11 @@ export default class ReviewForm extends React.Component {
         'x-access-token': token,
         'Content-Type': 'application/json'
       },
-
       body: JSON.stringify(review)
     })
       .then(response => response.json())
       .then(result => {
-
+        window.location.hash = '#';
       });
   }
 
@@ -58,28 +58,39 @@ export default class ReviewForm extends React.Component {
     });
   }
 
+  handleCancelTripReview() {
+    event.preventDefault();
+    window.location.hash = '#';
+  }
+
   render() {
     return (
       <div className='container'>
-        <div className='row'>
-        <form onSubmit={this.handleSubmit} name="reviewForm">
-        <label>
-          Choose Country
-          <select value={this.state.country.countryId} onChange={this.handleChange}>
-            <option></option>
-            {
-              this.state.countries.map(country =>
-                <option key={country.countryId}>{country.name}</option>)
-            }
-          </select>
-          <input type="text" placeholder='city' name='city'>
-          </input>
-          <textarea rows="10" cols="50" name="review"></textarea>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <Navbar />
+        <div className='row centered padding-top20vh'>
+          <form className ="review-form" onSubmit={this.handleSubmit} name="reviewForm">
+            <label className="review-form-label">Country</label>
+            <br />
+              <select className="select-element" value={this.state.country.countryId} onChange={this.handleChange}>
+                <option></option>
+                {
+                  this.state.countries.map(country =>
+                  <option key={country.countryId}>{country.name}</option>)
+                }
+              </select>
+              <br />
+              <label className="review-form-label">City</label>
+              <br />
+              <input className="form-input-element column-width100" type="text" placeholder='city' name='city'></input>
+              <br />
+              <label className='review-form-label'>Review</label>
+              <textarea className='column-width100' rows="20" name="review"></textarea>
+          <button className='confirm-form-button height-2rem'>Confirm</button>
+           <button className='cancel-form-button' onClick={this.handleCancelTripReview}>Cancel</button>
+        </form>
         </div>
-      </div>
+        </div>
+
     );
   }
 }
