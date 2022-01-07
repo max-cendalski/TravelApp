1405c763e90ea36c23a83635a015946d3e43df78
Korneliu <olbiton@gmail.com>
2022-01-07 15:22:50 -0800

    finished styling form element

--- client/pages/review-form.jsx
+++ client/pages/review-form.jsx
@@ -80,28 +80,49 @@ export default class ReviewForm extends React.Component {
     return (
       <div className='container'>
         <Navbar />
-        <div className='row centered padding-top20vh'>
-          <form className ="review-form" onSubmit={this.handleSubmit} name="reviewForm">
-            <label className="review-form-label">Country</label>
-            <br />
-            <select className="select-element" value={this.state.countryId} onChange={this.handleChange} required>
-              <option></option>
-              {
-                this.state.countries.map(country =>
-                <option key={country.countryId} value={country.countryId}>{country.name}</option>)
-              }
-            </select>
-            <br />
-            <label className="review-form-label">City</label>
-            <br />
-            <input className="form-input-element column-width100" onChange={this.handleCityInput} type="text" placeholder='city' name='city' required></input>
-            <br />
-            <label className='review-form-label'>Review</label>
-            <textarea className='column-width100' onChange={this.handleTextarea}rows="20" name="review" required></textarea>
-            <button className='confirm-form-button height-2rem'>Confirm</button>
-            <button className='cancel-form-button' onClick={this.handleCancelTripReview}>Cancel</button>
-          </form>
-        </div>
+          <div className='row centered padding-top15vh'>
+            <form className ="review-form" onSubmit={this.handleSubmit} name="reviewForm">
+              <div className='row column-width100'>
+                <div className='review-form-left column-width50'>
+                  <div>
+                    <label className="review-form-label">Country</label>
+                    <br />
+                    <select className="select-element" value={this.state.countryId} onChange={this.handleChange} required>
+                    <option></option>
+                    {
+                      this.state.countries.map(country =>
+                      <option key={country.countryId} value={country.countryId}>{country.name}</option>)
+                    }
+                    </select>
+                  </div>
+                  <label className="review-form-label">City</label>
+                  <br />
+                  <input className="form-input-element" onChange={this.handleCityInput} type="text" placeholder='city' name='city' required></input>
+                </div>
+                <div className='review-form-right column-width50'>
+                  <h3>Your score from 0 to 100</h3>
+                  <input className="review-score-input" max="100" type="number" required></input>
+                  <label className="review-score-label">Things To Do</label>
+                  <br />
+                  <input className="review-score-input" max="100" type="number" required></input>
+                  <label className="review-score-label">Food</label>
+                  <br />
+                  <input className="review-score-input" max="100" type="number" required></input>
+                  <label className="review-score-label">People</label>
+                  <br />
+                  <input className="review-score-input" max="100" type="number" required></input>
+                  <label className="review-score-label">Transport</label>
+                  <br />
+                  <input className="review-score-input" max="100" type="number" required></input>
+                  <label className="review-score-label">Safety</label>
+                </div>
+              </div>
+              <label className='review-form-label'>Review</label>
+              <textarea className='column-width100' onChange={this.handleTextarea}rows="20" name="review" required></textarea>
+              <button className='confirm-form-button height-2rem'>Confirm</button>
+              <button className='cancel-form-button' onClick={this.handleCancelTripReview}>Cancel</button>
+            </form>
+          </div>
       </div>
     );
   }
