18a2bd70e654b006ffc6aa2a18cf85ac319d25bd
Korneliu <olbiton@gmail.com>
2022-03-02 20:38:27 -0800

    styled logout info

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -138,7 +138,7 @@ export default class Navbar extends React.Component {
           }
           </div>
           <div className={this.context.logoutInfo}>
-            <h2>Are you sure want to log out?</h2>
+            <h2>Are you sure you want to log out?</h2>
             <button onClick={this.context.handleConfirmLogout} className='button-confirm-logout'>Confirm</button>
             <button onClick={this.context.handleCancelLogout} className='button-cancel-logout'>Cancel</button>
           </div>
