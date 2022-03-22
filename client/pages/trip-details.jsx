65b3b753a944beab19c18e9979117ca4f4c8d4e8
Korneliu <olbiton@gmail.com>
2022-03-21 20:53:51 -0700

    added classes from state

--- client/pages/trip-details.jsx
+++ client/pages/trip-details.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import Navbar from '../components/navbar';
 import AppContext from '../lib/app-context';
 import parseRoute from '../lib/parse-route';
+import EditReview from './edit-review';
 
 export default class TripDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      trip: null
+      trip: null,
+      review: 'container',
+      editReview: 'hidden'
+
     };
     this.handleEditButton = this.handleEditButton.bind(this);
   }
 
   handleEditButton() {
-    console.log('whee');
+    this.setState({
+      review: 'hidden',
+      editReview: 'container'
+    });
   }
 
   componentDidMount() {
@@ -38,96 +45,45 @@ export default class TripDetails extends React.Component {
     } = this.state.trip;
     return (
       <>
-      <div className='container'>
-        <Navbar handleChange={this.handleChange}
-                searchBox={this.state.searchBox}
-        />
-        <div className='row padding-top3 detailed-view-container'>
-          <div className='image-container column-width50'>
-            <img className="photo" src={mainPhotoUrl} alt={cityName}></img>
-          </div>
-
-          <div className='horizontal column-width50'>
-            <div className='column-width25'>
-              <div className='score-container'>
-                <p className='score-text'>Things to Do - {thingsTodoScore}</p>
-                <p className='score-text'>Food - {foodScore}</p>
-                <p className='score-text'>People - {peopleScore}</p>
-                <p className='score-text'>Transport - {transportScore}</p>
-                <p className='score-text'>Safety - {safetyScore}</p>
+        <div className={this.state.review}>
+          <Navbar handleChange={this.handleChange}
+                  searchBox={this.state.searchBox}
+          />
+            <div className='row padding-top3 detailed-view-container'>
+              <div className='image-container column-width50'>
+                <img className="photo" src={mainPhotoUrl} alt={cityName}></img>
               </div>
-            </div>
-            <div className='column-width25'>
-              <div className="detailed-text-container">
-                <p className='country-name'>{countryName}-<span className='city-name'>{cityName}</span></p>
-                <span className='city-name'>@{username}</span></div>
-            </div>
-          </div>
-        </div>
-        <div className='row detailed-view-container'>
+              <div className='horizontal column-width50'>
+                <div className='column-width25'>
+                  <div className='score-container'>
+                    <p className='score-text'>Things to Do - {thingsTodoScore}</p>
+                    <p className='score-text'>Food - {foodScore}</p>
+                    <p className='score-text'>People - {peopleScore}</p>
+                    <p className='score-text'>Transport - {transportScore}</p>
+                    <p className='score-text'>Safety - {safetyScore}</p>
+                  </div>
+                </div>
+                <div className='column-width25'>
+                  <div className="detailed-text-container">
+                    <p className='country-name'>{countryName}-<span className='city-name'>{cityName}</span></p>
+                    <span className='city-name'>@{username}</span></div>
+                  </div>
+                </div>
+              </div>
+            <div className='row detailed-view-container'>
             <div className='review-container'>
               <p>{review}</p>
+            </div>
+          </div>
+          <div>
+            {
+              this.context.user.username === username && <button onClick={this.handleEditButton} className='edit-form-button'>Edit</button>
+            }
           </div>
         </div>
-    <div>
-          {
-            this.context.user.username === username && <button onClick={this.handleEditButton} className='edit-form-button'>Edit</button>
-          }
+        <div className={this.state.editReview}>
+            <EditReview />
         </div>
-        </div>
-          <div className='container'>
-        <div className='row horizontal padding-top3 detailed-view-container'>
-          <div className='image-container column-width50'>
-            <img className="photo" src={mainPhotoUrl} alt={cityName}></img>
-          </div>
-
-          <form className='edit-form column-width50'>
-          <div className='column-width50'>
-              <div className='label-input-container'>
-              <label className='edit-score-label'>Country</label>
-              <input className='edit-form-text-input float-right' type='text' placeholder={countryName}></input>
-          </div>
-
-              <div className='label-input-container'>
-              <label className='edit-score-label'>City</label>
-              <input className='edit-form-text-input float-right' type='text' placeholder={cityName}></input>
-              </div>
-              <div className='label-input-container'>
-
-              <label className='edit-score-label'>Things to Do</label>
-              <input className='edit-form-text-input float-right' type="number" max="100" placeholder={thingsTodoScore}></input>
-           </div>
-              <div className='label-input-container'>
-
-              <label className='edit-score-label'>Food</label>
-              <input className='edit-form-text-input float-right' type="number" max="100" placeholder={foodScore}></input>
-              </div>
-              <div className='label-input-container'>
-
-              <label className='edit-score-label'>People</label>
-              <input className='edit-form-text-input float-right' type="number" max="100" placeholder={peopleScore}></input>
-              </div>
-              <div className='label-input-container'>
-
-              <label className='edit-score-label'>Transport</label>
-              <input className='edit-form-text-input float-right' type="number" max="100" placeholder={transportScore}></input>
-              </div>
-              <div className='label-input-container'>
-
-              <label className='edit-score-label'>Safety</label>
-              <input className='edit-form-text-input float-right' type="number" max="100" placeholder={safetyScore}></input>
-              </div>
-
-          </div>
-
-                <textarea className='edit-form-textarea'></textarea>
-
-            <button className='edit-form-submit-button'>Submit</button>
-          </form>
-
-        </div>
-        </div>
-
       </>
     );
   }
