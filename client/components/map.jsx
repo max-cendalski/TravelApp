e40bb1c9f46511155d8c28ee79b4dd875cca419b
Korneliu <olbiton@gmail.com>
2022-04-25 17:53:28 -0700

    set useEffect

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-import { useLayoutEffect } from 'react/cjs/react.production.min';
-
 function MapComponent(props) {
-
   const containerStyle = {
     width: props.width,
     top: '21rem',
@@ -12,6 +9,26 @@ function MapComponent(props) {
     left: '1rem',
     border: '1px solid rgb(240,131,52)'
   };
+  const [screen, setWidth] = useState({
+    width: window.innerWidth
+  });
+  useEffect(() => {
+    function handleResize() {
+      setWidth({
+        width: window.innerWidth
+      });
+    }
+    window.addEventListener('resize', handleResize);
+    if (screen.width < 660) {
+      containerStyle.width = '95%';
+    }
+    if (screen.width > 660) {
+      containerStyle.width = '38%';
+    }
+    return _ => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
 
   return (
     <div>
