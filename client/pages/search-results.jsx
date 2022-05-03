ffed82495f1bd3a8cd01190b03dd44e92ef4ee2a
Korneliu <olbiton@gmail.com>
2022-05-02 19:44:36 -0700

    initial commit

--- client/pages/search-results.jsx
+++ client/pages/search-results.jsx
@@ -22,6 +22,8 @@ export default class SearchResults extends React.Component {
           countries: result
         });
       });
+    /*    console.log('props.country', this.props.country);
+    console.log('this.stae.countrie', this.state.countries); */
   }
 
   componentDidUpdate(prevProps) {
@@ -40,6 +42,7 @@ export default class SearchResults extends React.Component {
           countries: result
         });
       });
+    // console.log('this.stae.countrie', this.state.countries);
   }
 
   render() {
