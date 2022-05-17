4b5260e851887c897fca7b1d7c8a03af970ce226
Korneliu <olbiton@gmail.com>
2022-05-16 17:05:13 -0700

    installed aws-sdk and multer s-3

--- server/uploads-middleware.js
+++ server/uploads-middleware.js
@@ -2,6 +2,11 @@ const path = require('path');
 const multer = require('multer');
 const imagesDirectory = path.join(__dirname, 'public/images');
 
+/* const s3 = new S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+}); */
+
 const storage = multer.diskStorage({
   destination(req, file, callback) {
     callback(null, imagesDirectory);
