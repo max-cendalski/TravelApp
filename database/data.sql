cfbc6d356aa9f40ce20dea7bcf076d377add63c1
Korneliu <olbiton@gmail.com>
2022-05-23 19:28:43 -0700

    removed images

--- database/data.sql
+++ database/data.sql
@@ -1,47 +1,6 @@
-insert into "users" ("username", "password")
+ insert into "users" ("username", "password")
 values
-('max','$argon2i$v=19$m=4096,t=3,p=1$Jq5BzV9Q6OmGyhYRaW1i0A$WuI1mJVIpJJyZiIi6GxnP3zhG13Np3jOsxcU2gQGp7U'),
-('max1','$argon2i$v=19$m=4096,t=3,p=1$RIIqQK6gVp7E41e+VF1OqA$uQalTlGbW3MsUFrdmhzD+8wvOKPUn1OOmwleP85eSJA'),
-('max2','$argon2i$v=19$m=4096,t=3,p=1$Tj30MSC0jfftI78mfYMFxw$qO6BX55FwQIXXLlkLGT043HFyFUWetV3E8MooOPSkmw'),
-('max3','$argon2i$v=19$m=4096,t=3,p=1$KXrcBRAGJ3AMz0THaR8jFA$ax/JR0lo4hUeKI6jRBYHJSvCc8u/A7RD4fANOIq6kHk');
-
-
-
-insert into "trips" ("userId","country","city","mainPhotoUrl","review","thingsTodoScore","foodScore","peopleScore","transportScore","safetyScore")
-values
-('1','greece','athens','/images/Greece2.jpg','Mayco, our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, and fill in the story behind the visuals.
-
-Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates.','90','66','88','39','89'),
-('2','japan','kyoto','/images/Japan2.jpg','Mayco, our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, andMayco, Mayco, our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, and fill in the story behind the visuals. Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates.our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, and fill in the story behind the visuals. Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates. fill in the story behind t
-m
-Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates.','93','34','44','34','84'),
-('2','tahiti','moorea','/images/Tahiti3.jpg','Mayco, our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, and fill in the story behind the visuals.
-
-Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates.','94','34','44','34','84'),
-('3','new zealand','montalto','/images/NZ5.jpg','Mayco, our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, and fill in the story behind the visuals.
-
-Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates.','94','34','44','34','48'),
-('1','norway','trondheim','/images/Norway2.jpg','Mayco, our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, and fill in the story behind the visuals.
-
-Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates.','94','34','44','34','84'),
-('2','tahiti','moorea','/images/Tahiti2.jpg','Mayco, our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, and fill in the story behind the visuals.
-
-Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates.','94','34','44','34','84'),
-('2','tahiti','papeete','/images/Tahiti3.jpg','Mayco, our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, and fill in the story behind the visuals.
-
-Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates.','94','34','44','34','83'),
-('3','tahiti','moorea','/images/tahitiMooreaSmall4.jpg','Mayco, our guide for this 75-minute tour, tells us of Kyoto’s past as the capital of Japan from 794 to 1869, remarking that some people still consider it to be the nation’s capital over Tokyo. She goes on to illustrate several of Kyoto’s cultural offerings with a wide selection of photos and on-the-ground video footage, pausing periodically to engage her audience with questions, and fill in the story behind the visuals.
-
-Using this hybrid presentation style, Mayco guides us into the shrine complex. We follow clusters of visitors along the wide, maple-lined walking path and up a set of stairs, catching a glimpse of the red-and-green accented building that houses the main shrine before passing through another large torii gate. Here we get our first look at one of the most famous views in Japan: the Senbon Torii, or 1000 gates.','93','35','34','38','38');
-
-
-insert into "comments" ("userId", "tripId","content")
-values
-('1','3','I really enjoyed reading your review.'),
-('3','3','Great Review'),
-('4','3','Thank you for a great review');
-
-insert into "tripScores" ("userId","tripId","score")
-values
-('2','8','100'),
-('4','8','100');
+('max','$argon2i$v=19$m=16,t=2,p=1$cUdBQUJRYnF2akRva3BqbA$qx31xcW91hecjTN4n3UBkeflmKvG8yAPwTYk+JilMoQ'),
+('edward','$argon2i$v=19$m=16,t=2,p=1$czJFVlZCZVRoVmdKQ2t5NQ$uUvwFrJu0KHhJf6ng31Ok/2VHMdtSg6ieROJm1Do8jc'),
+('tom','$argon2i$v=19$m=16,t=2,p=1$RU9JRkkyYzNKQjFlcTZ6MA$ReVMWBd7Zy9QPDzsS0yx9aQYKMfgIvJPKiKFo95pIB8'),
+('jennifer','$argon2i$v=19$m=16,t=2,p=1$eUJoRGl5STdZWlJ2Q0NMcQ$0tQbkRry1jOC1AgPm/Y7NY/oyr1dBtl7K63r0WgY7i0');
