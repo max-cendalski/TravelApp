4e13158338992c367b1fdbc116d9cc690c81e43d
Korneliu <olbiton@gmail.com>
2023-02-15 19:47:51 -0800

    conversed change and submit

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -1,24 +1,195 @@
-import React from 'react';
-import SignUpForm from '../components/sign-up-form';
-import SignInForm from '../components/sign-in-form';
-import {useState, useEffect} from 'react';
-import AppContext from '../lib/app-context';
+import React from "react";
+import SignUpForm from "../components/sign-up-form";
+import SignInForm from "../components/sign-in-form";
+import { useState, useEffect } from "react";
+import AppContext from "../lib/app-context";
 
+const Navbar = () => {
+  const [searchBox, setSearchBox] = useState("");
+  const [visible, setVisible] = useState("hidden");
+  const [modal, setModal] = useState("hidden");
+  const [signUpForm, setSignUpForm] = useState(false);
+  const [signInForm, setSignInForm] = useState(false);
+  const [searchArray, setSearchArray] = useState([]);
 
+  const handleChange = (event) => {
+    event.preventDefault();
+    const letter = event.target.value;
+    setSearchBox(letter);
+    const locationsArray = [];
+    if (letter === "") {
+      setSearchArray([]);
+    } else {
+      this.context.locations.forEach((location) => {
+        if (
+          location.country.includes(letter.toLowerCase()) ||
+          location.city.includes(letter.toLowerCase)
+        ) {
+          locationsArray.push(location);
+          const filteredLocations = locationsArray.filter(
+            (location, index, array) =>
+              index ===
+              array.findIndex(
+                (item) =>
+                  item.country === location.country &&
+                  item.city === location.city
+              )
+          );
+          setSearchArray(filteredLocations);
+        }
+      });
+    }
+  };
 
-/* const Navbar = ()=> {
-  const [searchBox, setSearchBox]  = useState('')
-  const [visible, setVisible] = useState('hidden');
-  const [modal, setModal]  = useState('hidden');
-  const [signUpForm, setSignUpForm] = useState(false)
-  const [signInForm, setSignInForm] = useState(false)
-  const [searchArray, setSearchArray] = useState([])
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const country = searchBox.split(",")[0];
+    window.location.hash = `#search-results?country=${country}`;
+    setSearchBox("");
+    setSearchArray("");
+  };
 
+  return (
+    <article
+      className="navbar-container row"
+      onMouseLeave={this.handleOnMouseLeave}
+    >
+      <section className="navbar-search-box-container">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="search"
+            value={this.state.searchBox}
+            onChange={this.handleChange}
+            autoComplete="off"
+            name="searchBox"
+            placeholder="search for a country"
+            required
+          />
+          <button className="submit-search-button">Submit</button>
+        </form>
+        <section>
+          <ul id="search-result-list">
+            {this.state.searchArray &&
+              this.state.searchArray.map((location, index) => {
+                return (
+                  <li
+                    onClick={this.handleSearchListClick}
+                    className="search-result-list-item"
+                    data-country={location.country}
+                    data-city={location.city}
+                    key={index}
+                  >
+                    {location.country}, {location.city}
+                  </li>
+                );
+              })}
+          </ul>
+        </section>
+      </section>
 
-} */
+      <section className="navbar-app-home">
+        <h1>
+          <a href="#">TravelApp</a>
+        </h1>
+      </section>
+      <section className="navbar-app-mobile">
+        <h1>
+          <a href="#">
+            <p>Travel</p>
+            <p>App</p>
+          </a>
+        </h1>
+      </section>
 
+      <section
+        className="navbar-login-icon-section"
+        onMouseEnter={this.handleOnMouseEnter}
+      >
+        {this.context.user && (
+          <p className="navbar-name-paragraph">
+            Hello, {this.context.user.username}
+          </p>
+        )}
+        <i className="fas fa-user icon-class" />
+      </section>
 
-export default class Navbar extends React.Component {
+      <section className={this.state.visible}>
+        <ul className="drop-down-list">
+          {!this.context.user && (
+            <li className="sign-up-button" onClick={this.handleSignUp}>
+              Sign Up
+            </li>
+          )}
+          {!this.context.user && (
+            <li className="sign-in-button" onClick={this.handleSignInButton}>
+              Sign In
+            </li>
+          )}
+          {this.context.user && (
+            <li
+              className="my-reviews-link"
+              onClick={this.handleMyReviewsButton}
+            >
+              My Reviews
+            </li>
+          )}
+          {this.context.user && (
+            <li className="write-review-button">
+              <a className="write-review-link" href="#review-form">
+                Write Review
+              </a>
+            </li>
+          )}
+          {this.context.user && (
+            <li
+              className="logoutButton"
+              onClick={this.context.handleLogoutWindow}
+            >
+              Logout
+            </li>
+          )}
+        </ul>
+      </section>
+
+      <article className={this.state.modal}>
+        {this.state.signUpForm && (
+          <section className="row">
+            <SignUpForm
+              handleSwitchingModal={this.handleSwitchingModal}
+              handleIsAuthorizing={this.handleIsAuthorizing}
+            />
+          </section>
+        )}
+        {this.state.signInForm && (
+          <section className="row">
+            <SignInForm
+              handleSwitchingModal={this.handleSwitchingModal}
+              handleIsAuthorizing={this.handleIsAuthorizing}
+            />
+          </section>
+        )}
+      </article>
+      <article className={this.context.logoutInfo}>
+        <h2>Are you sure you want to logout?</h2>
+        <button
+          onClick={this.context.handleConfirmLogout}
+          className="app-button background-orange float-right"
+        >
+          Confirm
+        </button>
+        <button
+          onClick={this.context.handleCancelLogout}
+          className="app-button background-red"
+        >
+          Cancel
+        </button>
+      </article>
+    </article>
+  );
+};
+Navbar.contextType = AppContext;
+
+/* export default class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = ({
@@ -71,6 +242,8 @@ export default class Navbar extends React.Component {
       });
     }
   }
+
+  //
 
   handleSubmit(event) {
     event.preventDefault();
@@ -218,6 +391,4 @@ export default class Navbar extends React.Component {
       </article>
     );
   }
-}
-
-Navbar.contextType = AppContext;
+} */
