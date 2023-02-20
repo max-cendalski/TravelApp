cb479aaf33ead0b559a42f458846634aa1e21306
Korneliu <olbiton@gmail.com>
2023-02-19 16:56:49 -0800

    removed outcommented code

--- client/components/sign-in-form.jsx
+++ client/components/sign-in-form.jsx
@@ -1,49 +1,47 @@
-import React from "react";
-import AppContext from "../lib/app-context";
-import { useState, useContext } from "react";
-import { AppDataContext } from "./context";
+import React, { useState, useContext } from 'react';
+import { AppDataContext } from './context';
 
-const SignInForm = (props) => {
+const SignInForm = props => {
   const SingInContextData = useContext(AppDataContext);
   const [userInfo, setUserInfo] = useState({
-    username: "",
-    password: "",
+    username: '',
+    password: ''
   });
-  const [errroMsg, setErrorMsg] = useState("invisible");
+  const [errroMsg, setErrorMsg] = useState('invisible');
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     const name = e.target.name;
     const value = e.target.value;
 
-    setUserInfo((prevData) => ({
+    setUserInfo(prevData => ({
       ...prevData,
-      [name]: value,
+      [name]: value
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     const user = { username: userInfo.username, password: userInfo.password };
 
-    fetch("/api/auth/sign-in", {
-      method: "POST",
+    fetch('/api/auth/sign-in', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(user),
+      body: JSON.stringify(user)
     })
-      .then((response) => response.json())
-      .then((result) => {
+      .then(response => response.json())
+      .then(result => {
         if (result.error) {
-          setErrorMsg("row");
+          setErrorMsg('row');
         } else {
           SingInContextData.handleSignIn(result);
-          window.location.hash = "";
+          window.location.hash = '';
           props.handleSwitchingModal();
         }
       })
-      .catch((error) => {
-        console.error("Error", error.message);
+      .catch(error => {
+        console.error('Error', error.message);
       });
   };
 
@@ -91,83 +89,3 @@ const SignInForm = (props) => {
 };
 
 export default SignInForm;
-/*
-export default class SignInForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-      errroMsg: 'invisible'
-    };
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-
-
-  handleUsernameChange(event) {
-    this.setState({
-      username: event.target.value
-    });
-  }
-
-  handlePasswordChange(event) {
-    this.setState({
-      password: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const user = {
-      username: this.state.username,
-      password: this.state.password
-    };
-    fetch('/api/auth/sign-in', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-      .then(response => response.json())
-      .then(result => {
-        if (result.error) {
-          this.setState({
-            errroMsg: 'row'
-          });
-        } else {
-          this.context.handleSignIn(result);
-          window.location.hash = '';
-          this.props.handleSwitchingModal();
-        }
-      })
-      .catch(error => {
-        console.error('Error', error.message);
-      });
-  }
-
-  render() {
-    return (
-          <article>
-          <section className={this.state.errroMsg}>
-              <h1 className='username-exists-msg'>Incorrect username or password!</h1>
-            </section>
-            <form className='sign-form' onSubmit={this.handleSubmit} name="signInForm">
-            <p>
-              <label className="form-label"> Sign In </label>
-              <input required className="username-input input-form" type="text" value={this.state.username} onChange={this.handleUsernameChange} name="username" placeholder="username"/>
-            </p>
-            <p>
-              <input required className="password-input input-form" type="password" value={this.state.password} onChange={this.handlePasswordChange} name="password" placeholder="password" />
-            </p>
-            <button className='app-button background-orange float-right'>Confirm</button>
-            <button className='app-button background-red' onClick={this.props.handleSwitchingModal}>Cancel</button>
-          </form>
-          </article>
-    );
-  }
-}
- */
--- client/components/sign-up-form.jsx
+++ client/components/sign-up-form.jsx
@@ -1,48 +1,46 @@
-import React from "react";
-import { useState, useContext } from "react";
-import { AppDataContext } from "./context";
+import React, { useState } from 'react';
 
-const SignUpForm = (props) => {
+const SignUpForm = props => {
   const [newUser, setNewUser] = useState({
-    username: "",
-    password: "",
+    username: '',
+    password: ''
   });
-  const [userExiststMsg, setUserExistsMsg] = useState("invisible");
+  const [userExiststMsg, setUserExistsMsg] = useState('invisible');
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     const name = e.target.name;
     const value = e.target.value;
 
-    setNewUser((prevData) => ({
+    setNewUser(prevData => ({
       ...prevData,
-      [name]: value,
+      [name]: value
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     const user = {
       username: newUser.username.toLowerCase(),
       password: newUser.password
     };
-     fetch("/api/auth/sign-up", {
-      method: "POST",
+    fetch('/api/auth/sign-up', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(user),
+      body: JSON.stringify(user)
     })
-      .then((response) => response.json())
-      .then((result) => {
+      .then(response => response.json())
+      .then(result => {
         if (result.error) {
-          setUserExistsMsg("row");
+          setUserExistsMsg('row');
         } else {
-          window.location.hash = "";
+          window.location.hash = '';
           props.handleSwitchingModal();
         }
       })
-      .catch((error) => {
-        console.error("Error", error.message);
+      .catch(error => {
+        console.error('Error', error.message);
       });
   };
 
@@ -99,119 +97,4 @@ const SignUpForm = (props) => {
   );
 };
 
-
 export default SignUpForm;
-/* export default class SignUpForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: "",
-      password: "",
-      userExiststMsg: "invisible",
-    };
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleUsernameChange(event) {
-    this.setState({
-      username: event.target.value.toLowerCase(),
-      userExiststMsg: "invisible",
-    });
-  }
-
-  handlePasswordChange(event) {
-    this.setState({
-      password: event.target.value,
-    });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const user = {
-      username: this.state.username,
-      password: this.state.password,
-    };
-    fetch("/api/auth/sign-up", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-    })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.error) {
-          this.setState({
-            userExiststMsg: "row",
-          });
-        } else {
-          window.location.hash = "";
-          this.props.handleSwitchingModal();
-        }
-      })
-      .catch((error) => {
-        console.error("Error", error.message);
-      });
-  }
-
-  render() {
-    return (
-      <article>
-        <section className={this.state.userExiststMsg}>
-          <h1 className="username-exists-msg">
-            User with that username already exists!
-          </h1>
-        </section>
-        <form
-          className="sign-form"
-          onSubmit={this.handleSubmit}
-          name="signUpForm"
-        >
-          <p>
-            <label className="form-label"> Sign up </label>
-            <input
-              required
-              className="username-input input-form"
-              type="text"
-              value={this.state.username}
-              onChange={this.handleUsernameChange}
-              name="username"
-              placeholder="username"
-              minLength="3"
-            />
-          </p>
-          <p className="password-notification">
-            username must me be at least 3 characters long
-          </p>
-          <p>
-            <input
-              required
-              className="password-input input-form"
-              type="password"
-              value={this.state.password}
-              onChange={this.handlePasswordChange}
-              name="password"
-              placeholder="password"
-              minLength="6"
-            />
-          </p>
-          <p className="password-notification">
-            password must be at least 6 characters long
-          </p>
-          <button className="app-button background-orange float-right">
-            Confirm
-          </button>
-          <button
-            className="app-button background-red"
-            onClick={this.props.handleSwitchingModal}
-          >
-            Cancel
-          </button>
-        </form>
-      </article>
-    );
-  }
-}
- */
