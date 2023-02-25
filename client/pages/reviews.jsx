5658e579a486cc648a43e7cb813cafad432de3df
Korneliu <olbiton@gmail.com>
2023-02-24 21:33:19 -0800

    added handleDeleteReview

--- client/pages/reviews.jsx
+++ client/pages/reviews.jsx
@@ -1,55 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from '../components/navbar';
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/navbar";
 
 const Reviews = () => {
   const [myReviews, setMyReviews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const token = localStorage.getItem('TravelApp-token');
-    fetch('/api/my-reviews', {
-      method: 'GET',
+    const token = localStorage.getItem("TravelApp-token");
+    fetch("/api/my-reviews", {
+      method: "GET",
       headers: {
-        'X-Access-Token': token,
-        'Content-Type': 'application/json'
-      }
+        "X-Access-Token": token,
+        "Content-Type": "application/json",
+      },
     })
-      .then(response => response.json())
-      .then(result => {
+      .then((response) => response.json())
+      .then((result) => {
         setMyReviews(result);
         setIsLoading(false);
       });
   }, []);
 
+  const handleDeleteReview = (id) => {
+    console.log("id", id);
+  };
+
   if (isLoading) return null;
   return (
     <article>
-      {myReviews.length > 0
-        ? (
+      {myReviews.length > 0 ? (
         <article>
           <Navbar />
           <section className="list-flex">
-            {myReviews.map(trip => (
+            {myReviews.map((trip) => (
               <div className="image-item column-width50" key={trip.tripId}>
-                <Trip trip={trip} />
+                <Trip trip={trip} handleDeleteReview={handleDeleteReview} />
               </div>
             ))}
           </section>
         </article>
-          )
-        : (
+      ) : (
         <article>
           <Navbar />
           <h1 className="nothing-found-msg">
             You don &apos;t have any reviews!
           </h1>
         </article>
-          )}
+      )}
     </article>
   );
 };
 
 function Trip(props) {
+
   const { tripId, country, city, mainPhotoUrl } = props.trip;
   return (
     <a href={`#trips?tripId=${tripId}`}>
@@ -58,7 +61,10 @@ function Trip(props) {
           {country}-<span className="city-name">{city}</span>
         </p>
       </section>
-      <section className="trash-container">
+      <section
+        onClick={() => props.handleDeleteReview(tripId)}
+        className="trash-container"
+      >
         <i className="fa-solid fa-trash fa-xl"></i>
       </section>
 
