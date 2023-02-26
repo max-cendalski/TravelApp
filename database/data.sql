7929ad81821618f84691ef1bd3551c94835b7691
Korneliu <olbiton@gmail.com>
2023-02-26 11:29:39 -0800

    added pseudo classes to text-container

--- database/data.sql
+++ database/data.sql
@@ -1,4 +1,4 @@
- /* insert into "users" ("username", "password")
+ insert into "users" ("username", "password")
 values
 ('max','$argon2i$v=19$m=16,t=2,p=1$cUdBQUJRYnF2akRva3BqbA$qx31xcW91hecjTN4n3UBkeflmKvG8yAPwTYk+JilMoQ'),
 ('edward','$argon2i$v=19$m=16,t=2,p=1$czJFVlZCZVRoVmdKQ2t5NQ$uUvwFrJu0KHhJf6ng31Ok/2VHMdtSg6ieROJm1Do8jc'),
@@ -85,4 +85,3 @@ values
 ('5','6','88'),
 ('5','5','70'),
 ('4','5','93');
- */
