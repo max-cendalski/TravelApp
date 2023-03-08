eaa10d56ecdf08fcb483305ff62ce50650cbedac
Korneliu <olbiton@gmail.com>
2023-03-07 19:39:44 -0800

    fixed carousel

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -17,7 +17,6 @@ const Home = () => {
       .then((result) => {
         const images = [];
         const countries = [];
-
         for (let i = 0; i < 6; i++) {
           images.push(result[i].mainPhotoUrl);
           countries.push(result[i].country);
@@ -54,6 +53,18 @@ const Home = () => {
         <div className="image-home">
           <p className="carousel-country-name">{countriesCarousel[2]}</p>
           <img className="photo" src={imagesCarousel[2]} />
+        </div>
+        <div className="image-home">
+          <p className="carousel-country-name">{countriesCarousel[3]}</p>
+          <img className="photo" src={imagesCarousel[3]} />
+        </div>
+        <div className="image-home">
+          <p className="carousel-country-name">{countriesCarousel[4]}</p>
+          <img className="photo" src={imagesCarousel[4]} />
+        </div>
+        <div className="image-home">
+          <p className="carousel-country-name">{countriesCarousel[5]}</p>
+          <img className="photo" src={imagesCarousel[6]} />
         </div>
       </Carousel>
     </article>
