f79c328b03c9b3940d613e68f453c069df4dd02b
Korneliu <olbiton@gmail.com>
2023-03-07 20:00:05 -0800

    added weather api do .env

--- client/components/weather.jsx
+++ client/components/weather.jsx
@@ -2,6 +2,11 @@ import React{useState, useEffect} from 'react';
 
 const Weather = () => {
 
+  useEffect(()=> {
+    const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+  })
+
+
   return (
     <article>
       <p>66&deg</p>
