464bbb751cd49d14b5da2ee0a4832efb4806a01e
Korneliu <olbiton@gmail.com>
2023-03-13 20:40:32 -0700

    changed  sign up form position

--- client/components/sign-up-form.jsx
+++ client/components/sign-up-form.jsx
@@ -5,15 +5,15 @@ const SignUpForm = (props) => {
     username: "",
     password: "",
   });
-  const [userExiststMsg, setUserExistsMsg] = useState("invisible");
-  const [incorrectLoginMsg, setIncorrectLoginMsg] = useState("invisible");
+  const [userExiststMsg, setUserExistsMsg] = useState("hidden");
+  const [incorrectLoginMsg, setIncorrectLoginMsg] = useState("hidden");
 
   const handleChange = (e) => {
-    console.log(newUser.username.length)
-    let letters = /^[A-Za-z]+$/;
+    let letters = /^[A-Za-z,0-9]+$/;
     function checkLetters(input) {
-      if ((input.match(letters)) || (input.length === 0 )) {
+      if (input.match(letters) || input.length === 0) {
         setIncorrectLoginMsg("hidden");
+        console.log(newUser.username);
       } else {
         setIncorrectLoginMsg("incorrect-login-msg");
       }
@@ -31,12 +31,13 @@ const SignUpForm = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    function checkUsername(userData) {}
+    checkUsername(newUser);
     const user = {
       username: newUser.username,
       password: newUser.password,
     };
-    console.log('user',user)
-  /*   fetch("/api/auth/sign-up", {
+    /*   fetch("/api/auth/sign-up", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -60,55 +61,57 @@ const SignUpForm = (props) => {
   return (
     <article>
       <section className={userExiststMsg}>
-        <h1 className="username-exists-msg">
-          User with that username already exists!
-        </h1>
+        User with that username already exists!
       </section>
       <section className={incorrectLoginMsg}>
-        Username can contains only letters and max two numbers
+        Username can contains only letters and maximum two numbers
       </section>
-      <form className="sign-form" onSubmit={handleSubmit} name="signUpForm">
-        <p>
-          <label className="form-label"> Sign up </label>
-          <input
-            required
-            className="username-input input-form"
-            type="text"
-            value={newUser.username}
-            onChange={handleChange}
-            name="username"
-            placeholder="username"
-            minLength="3"
-          />
-        </p>
-        <p className="password-notification">
-          username must me be at least 4 characters long
-        </p>
-        <p>
-          <input
-            required
-            className="password-input input-form"
-            type="password"
-            value={newUser.password}
-            onChange={handleChange}
-            name="password"
-            placeholder="password"
-            minLength="6"
-          />
-        </p>
-        <p className="password-notification">
-          password must be at least 6 characters long
-        </p>
-        <button className="app-button background-orange float-right">
-          Confirm
-        </button>
-        <button
-          className="app-button background-red"
-          onClick={props.handleSwitchingModal}
-        >
-          Cancel
-        </button>
-      </form>
+      <section className="sign-form">
+        <form onSubmit={handleSubmit} name="signUpForm">
+          <p>
+            <label className="form-label"> Sign up </label>
+            <input
+              required
+              className="username-input input-form"
+              type="text"
+              value={newUser.username}
+              onChange={handleChange}
+              name="username"
+              placeholder="username"
+              minLength="4"
+              maxLength="15"
+            />
+          </p>
+          <p className="password-notification">
+            Username must me be at least 4 characters long
+          </p>
+          <p>
+            <input
+              required
+              className="password-input input-form"
+              type="password"
+              value={newUser.password}
+              onChange={handleChange}
+              name="password"
+              placeholder="password"
+              minLength="6"
+              maxLength="20"
+            />
+          </p>
+          <p className="password-notification">
+            password must be at least 6 characters long
+          </p>
+          <button className="app-button background-orange float-right">
+            Confirm
+          </button>
+          <button
+            className="app-button background-red"
+            onClick={props.handleSwitchingModal}
+          >
+            Cancel
+          </button>
+        </form>
+      </section>
     </article>
   );
 };
